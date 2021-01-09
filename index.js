d3d48a1aaac333767fa2b46883516e1fe655ce34
esarnb <esarnb@gmail.com>
2021-01-09 00:59:03 -0500

    theme switch works only once...

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ $(document).ready(function() {
 
 
     $("#theme-btn").click(() => {
-        console.log("btn clicked");
+        console.log($("#theme-text").text());
+
         // Fetch all classes of a certain div
         //document.getElementById('divId').className.split(/\s+/);
-        var classList = $('#theme-text').attr('class').split(/\s+/);
+        // var classList = $('#theme-text').attr('class').split(/\s+/);
         
-        // Switch between dark and light based on current theme-toggle[class] setting.
-        classList[0] === 'Dark' ? lightTheme() : darkTheme()
+        // Switch between dark and light based on current theme-text content.
+        $('#theme-text').text() === 'Dark' ? lightTheme() : darkTheme()
     })
     
 })
@@ -38,20 +39,32 @@ $(document).ready(function() {
 */
 
 function lightTheme() {
-    $("#theme-text").text("Light").attr("class", "Light")
+    $("#theme-text").text("Light")//.attr("class", "Light")
 
-    // .attr('style', function(i,s) { return (s || '')
+    // .attr('style', function(i,s) { return (s || '') + 
+
 
     $("#navHeader, .card").each(function(){ 
-        $(this).css({"background-color": "#9bd1bc !important"})
+        $(this).attr('style', function(i,s) { return (s || '') + "background-color: #9bd1bc !important"})
     }); 
     $("#page, .navbar-brand, .card").each(function(){ 
-        $(this).css({"color": "rgb(77, 48, 86) !important"})
+        $(this).attr('style', function(i,s) { return (s || '') + "color: rgb(77, 48, 86) !important"})
     }); 
     $("body, .child-page").each(function(){ 
-        $(this).css({"background-color": "#c9c6bd !important"})
+        $(this).attr('style', function(i,s) { return (s || '') + "background-color: #c9c6bd !important"})
     }); 
-    $(".card a").css({"color": "rgb(50, 25, 25) !important"})
+    $(".card a").attr('style', function(i,s) { return (s || '') + "color: rgb(50, 25, 25) !important"})
+
+    // $("#navHeader, .card").each(function(){ 
+    //     $(this).css({"background-color": "#9bd1bc !important"})
+    // }); 
+    // $("#page, .navbar-brand, .card").each(function(){ 
+    //     $(this).css({"color": "rgb(77, 48, 86) !important"})
+    // }); 
+    // $("body, .child-page").each(function(){ 
+    //     $(this).css({"background-color": "#c9c6bd !important"})
+    // }); 
+    // $(".card a").css({"color": "rgb(50, 25, 25) !important"})
 }
 
 
@@ -81,15 +94,26 @@ function lightTheme() {
 */
 
 function darkTheme() {
-    $("#theme-text").text("Dark").attr("class", "Dark")
+    $("#theme-text").text("Dark")//.attr("class", "Dark")
 
-    $("#navHeader").css({"background-color": "#1f2833 !important"})
+
+    $("#navHeader").attr('style', function(i,s) { return (s || '') + "background-color: #1f2833 !important"})
     $("#page, .navbar-brand").each(function(){ 
-        $(this).css({"color": "#42ddd3"})
+        $(this).attr('style', function(i,s) { return (s || '') + "color: #42ddd3"})
     }); 
     $("body, .child-page").each(function(){ 
-        $(this).css({"background-color": "#2c3531 !important"})
+        $(this).attr('style', function(i,s) { return (s || '') + "background-color: #2c3531 !important"})
     }); 
-    $(".card").css({"color": " rgb(90, 11, 126) !important", "background-color": "#8df0fba8 !important"})
-    $(".card a").css({"color": "rgb(50, 25, 25) !important"})
+    $(".card").attr('style', function(i,s) { return (s || '') + "color: rgb(90, 11, 126) !important; background-color: #8df0fba8 !important"})
+    $(".card a").attr('style', function(i,s) { return (s || '') + "color: rgb(50, 25, 25) !important"})
+
+    // $("#navHeader").css({"background-color": "#1f2833 !important"})
+    // $("#page, .navbar-brand").each(function(){ 
+    //     $(this).css({"color": "#42ddd3"})
+    // }); 
+    // $("body, .child-page").each(function(){ 
+    //     $(this).css({"background-color": "#2c3531 !important"})
+    // }); 
+    // $(".card").css({"color": " rgb(90, 11, 126) !important", "background-color": "#8df0fba8 !important"})
+    // $(".card a").css({"color": "rgb(50, 25, 25) !important"})
 }
