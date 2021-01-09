1e3e8e6b9fc4516dee1bf0a47ef456d943c5979d
esarnb <esarnb@gmail.com>
2021-01-09 00:48:23 -0500

    added toggle theme switch and light/dark jquery edit

--- index.js
+++ index.js
@@ -5,12 +5,91 @@ $(document).ready(function() {
         }, i * 250)
     })
 
-    // $("#proj-tab").onClick(() => {
-    //     $('.card').each(function(i) {
-    //         setTimeout(() => {
-    //             $(this).css("visibility", "visible").hide().fadeIn(1000);
-    //         }, i * 250)
-    //     })
-    // })
 
+    $("#theme-btn").click(() => {
+        console.log("btn clicked");
+        // Fetch all classes of a certain div
+        //document.getElementById('divId').className.split(/\s+/);
+        var classList = $('#theme-text').attr('class').split(/\s+/);
+        
+        // Switch between dark and light based on current theme-toggle[class] setting.
+        classList[0] === 'Dark' ? lightTheme() : darkTheme()
+    })
+    
 })
+/* Light Theme   */
+
+/*  
+    #navHeader, .card {
+        background-color: #9bd1bc !important;
+    }
+
+    #page, .navbar-brand, .card {
+        color: rgb(77, 48, 86) !important;
+    }
+
+    body, .child-page{
+        background-color: #c9c6bd !important;
+    }
+
+    .card a {
+        color: rgb(50, 25, 25);
+    }  
+*/
+
+function lightTheme() {
+    $("#theme-text").text("Light").attr("class", "Light")
+
+    // .attr('style', function(i,s) { return (s || '')
+
+    $("#navHeader, .card").each(function(){ 
+        $(this).css({"background-color": "#9bd1bc !important"})
+    }); 
+    $("#page, .navbar-brand, .card").each(function(){ 
+        $(this).css({"color": "rgb(77, 48, 86) !important"})
+    }); 
+    $("body, .child-page").each(function(){ 
+        $(this).css({"background-color": "#c9c6bd !important"})
+    }); 
+    $(".card a").css({"color": "rgb(50, 25, 25) !important"})
+}
+
+
+
+/*  Dark Theme  */
+/*     
+    #navHeader {
+        background-color: #1f2833 !important;
+    }
+
+    #page, .navbar-brand {
+        color: #42ddd3 !important;
+    }
+
+    body, .child-page{
+        background-color: #2c3531 !important;
+    }
+
+    .card {
+        background-color: #8df0fba8;
+        color: rgb(90, 11, 126);
+    }
+
+    .card a {
+        color: rgb(120, 42, 155);
+    } 
+*/
+
+function darkTheme() {
+    $("#theme-text").text("Dark").attr("class", "Dark")
+
+    $("#navHeader").css({"background-color": "#1f2833 !important"})
+    $("#page, .navbar-brand").each(function(){ 
+        $(this).css({"color": "#42ddd3"})
+    }); 
+    $("body, .child-page").each(function(){ 
+        $(this).css({"background-color": "#2c3531 !important"})
+    }); 
+    $(".card").css({"color": " rgb(90, 11, 126) !important", "background-color": "#8df0fba8 !important"})
+    $(".card a").css({"color": "rgb(50, 25, 25) !important"})
+}
