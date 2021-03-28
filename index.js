a5181832223b607ec357b2bb09504660469b476b
esarnb <esarnb@gmail.com>
2021-03-28 18:31:01 -0400

    generating cards through js with proj obj

--- index.js
+++ index.js
@@ -3,8 +3,31 @@ $(document).ready(function() {
     console.log("JQuery")
     
     $.getJSON('projects.json', function(data){
+        let container = $("#proj-cards-container");
+        
         $.each(data.projects, function(i, x){
-            console.log(x);
+            container.append(
+`
+<div class="ui card cards-light">
+    <div class="image">
+        <img src="/images/test/placeholder_kristy.png">
+    </div>
+    <div class="content">
+        <a class="header">Kristy</a>
+    <div class="meta">
+        <span>Joined in 2013</span>
+    </div>
+    <div class="description">
+        ${x.desc}
+    </div>
+    </div>
+    <div class="extra content">
+        <a href="${x.repo}"> <button> repo </button> </a>
+        <a href="${x.live}"> <button> live </button> </a>
+    </div>
+</div>
+`
+            )
         });
     }).catch((rejected) => console.log(rejected))
 })
