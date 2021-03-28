85e023c10a41d539d6ef3afb7740f82c0b9b19e5
esarnb <esarnb@gmail.com>
2021-03-28 17:46:34 -0400

    json data setup, imported data

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
 $(document).ready(function() {
-
     console.log("JQuery")
+    
+    $.getJSON('projects.json', function(data){
+        $.each(data.projects, function(i, x){
+            console.log(x);
+        });
+    }).catch((rejected) => console.log(rejected))
 })
